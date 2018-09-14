ded1736ebf18762fcf9b12238f0bcf6351c4d303
Anthony Pisciotta <apisciotta1@live.maryville.edu>
2018-09-13 21:35:18 -0500

    lab 1.4

--- Lab - Control Flow.playground/Pages/2. Exercise - If and If-Else Statements.xcplaygroundpage/Contents.swift
+++ Lab - Control Flow.playground/Pages/2. Exercise - If and If-Else Statements.xcplaygroundpage/Contents.swift
@@ -23,10 +23,10 @@ if dollars == 0 {
  */
 dollars = 105
 if dollars == 0 {
-    print("Sorry, kid. You're broke!")
-} else if dollars =>0 {
+    print("Sorry,kid. You're broke!")
+} else {
     print("You've got some spending money!")
-} else dollars =< 100 {
+}; if dollars < 100 {
     print("Looks to me like you're rich!")
 }
 
